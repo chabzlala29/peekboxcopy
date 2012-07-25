36d4ce4c7ec1b3cf675bd807b5c5e8285fd7435f
Mk <peekbox@gmail.com>
2012-07-25 09:52:21 +0800

    Update spec/controllers/profile/albums_controller_spec.rb

--- spec/controllers/profile/albums_controller_spec.rb
+++ spec/controllers/profile/albums_controller_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 describe Profile::AlbumsController do
 
   describe "GET 'index'" do
-    it "returns http success" do
+    it "returns http redirect" do
       get 'index'
-      response.should be_success
+      response.should be_redirect
     end
   end
 
