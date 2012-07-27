2d85329eb16c3dd11e4eefb28af3e753509bd9c9
Jay Ricky Villarante & Titus James Del Castillo <pair+jay+titus@peekbox.tv>
2012-07-27 10:07:11 +0800

    scope messages in factor.rb

--- spec/factory.rb
+++ spec/factory.rb
@@ -5,21 +5,18 @@ module Factory
     user3 = User.create!(:email => "test3@test.com", :username => "user3", :password => "password")
     user4 = User.create!(:email => "test4@test.com", :username => "user4", :password => "password")
    
+    user1.messages.create!(:friend => user2, :message => "sent 1 week ago", :created_at => 1.week.ago)
+    user1.messages.create!(:friend => user2, :message => "sent 3 days ago", :created_at => 3.days.ago)
+    user1.messages.create!(:friend => user2, :message => "sent yesterday", :created_at => 1.day.ago)
 
-    Message.create!(:user => user1, :friend => user2, :message => "sent 1 week ago", :created_at => 1.week.ago)
-    Message.create!(:user => user1, :friend => user2, :message => "sent 3 days ago", :created_at => 3.days.ago)
-    Message.create!(:user => user1, :friend => user2, :message => "sent yesterday", :created_at => 1.day.ago)
-
-
-    Friendship.create!(:user => user1, :friend => user2)
-    Friendship.create!(:user => user1, :friend => user3)
-    Friendship.create!(:user => user1, :friend => user4)
-   
-
+    user1.friendships.create!(:friend => user2)
+    user1.friendships.create!(:friend => user3)
+    user1.friendships.create!(:friend => user4)
   end
 
   def self.teardown
     User.delete_all
-    
+    Message.delete_all
+    Friendship.delete_all
   end
 end
