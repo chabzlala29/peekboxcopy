5929dcdbaee5eff0662e3b60a2b4fea3c92de27a
Titus James Del Castillo <pair+titus@peekbox.tv>
2012-08-01 17:04:42 +0800

    test collab push

--- app/models/user_message.rb
+++ app/models/user_message.rb
@@ -1,8 +1,5 @@
 class UserMessage < ActiveRecord::Base
-
-  belongs_to :messages, :class_name => "Message", :foreign_key => "message_id", :dependent => :destroy
+  belongs_to :message, :class_name => "Message", :foreign_key => "message_id"
   
   attr_accessible :type_message, :message_id, :friend_id, :user_id
-  
-  
 end
