74e3106e3b8ed343dfa0c894fea49cf5a40b52fb
xirukitepe <ssalamat.peg@gmail.com>
2012-08-06 15:51:05 +0800

    Fixed the missing favicon in other pages

--- db/migrate/20120806020728_default_user_message_to_unread.rb
+++ db/migrate/20120806020728_default_user_message_to_unread.rb
@@ -1,6 +1,6 @@
 class DefaultUserMessageToUnread < ActiveRecord::Migration
   def up
-    #change_column :user_messages, :status, :string, :default => "unread"
+    change_column :user_messages, :status, :string, :default => "unread"
     UserMessage.update_all(:status => :unread)
     add_index :user_messages, [:user_id, :status], :name => "user_id_status_idx"
   end
